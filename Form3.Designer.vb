0d2a371d949fd19258ca5b6047b659b05ffd7ead
clarence1979 <47097322+clarence1979@users.noreply.github.com>
2022-02-19 23:34:50 +1100

    OK

--- Form3.Designer.vb
+++ Form3.Designer.vb
@@ -27,6 +27,25 @@ Partial Class Form3
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.lblSubject = New System.Windows.Forms.Label()
+        Me.lblSeller = New System.Windows.Forms.Label()
+        Me.lblPPrice = New System.Windows.Forms.Label()
+        Me.lblPurchaser = New System.Windows.Forms.Label()
+        Me.lblSalePrice = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.lblRating = New System.Windows.Forms.Label()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton5 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton6 = New System.Windows.Forms.RadioButton()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
@@ -42,17 +61,18 @@ Partial Class Form3
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(478, 230)
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(631, 449)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(211, 88)
+        Me.Button1.Size = New System.Drawing.Size(368, 129)
         Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "Load Database, store in array and search record"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(235, 95)
+        Me.Label2.Location = New System.Drawing.Point(12, 59)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(39, 13)
         Me.Label2.TabIndex = 2
@@ -61,26 +81,242 @@ Partial Class Form3
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(151, 356)
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(78, 148)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.Size = New System.Drawing.Size(222, 31)
         Me.Label3.TabIndex = 3
-        Me.Label3.Text = "Label3"
+        Me.Label3.Text = "Textbook name:"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(154, 397)
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(78, 209)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.Size = New System.Drawing.Size(112, 31)
         Me.Label4.TabIndex = 4
-        Me.Label4.Text = "Label4"
+        Me.Label4.Text = "Subject"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(78, 264)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(89, 31)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Seller"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(78, 308)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(212, 31)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "Purchase Price"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(78, 349)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(147, 31)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "Purchaser"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(78, 395)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(147, 31)
+        Me.Label8.TabIndex = 8
+        Me.Label8.Text = "Sale Price"
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Location = New System.Drawing.Point(509, 163)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(37, 13)
+        Me.lblTitle.TabIndex = 9
+        Me.lblTitle.Text = "lblTitle"
+        '
+        'lblSubject
+        '
+        Me.lblSubject.AutoSize = True
+        Me.lblSubject.Location = New System.Drawing.Point(501, 212)
+        Me.lblSubject.Name = "lblSubject"
+        Me.lblSubject.Size = New System.Drawing.Size(53, 13)
+        Me.lblSubject.TabIndex = 10
+        Me.lblSubject.Text = "lblSubject"
+        '
+        'lblSeller
+        '
+        Me.lblSeller.AutoSize = True
+        Me.lblSeller.Location = New System.Drawing.Point(503, 262)
+        Me.lblSeller.Name = "lblSeller"
+        Me.lblSeller.Size = New System.Drawing.Size(43, 13)
+        Me.lblSeller.TabIndex = 11
+        Me.lblSeller.Text = "lblSeller"
+        '
+        'lblPPrice
+        '
+        Me.lblPPrice.AutoSize = True
+        Me.lblPPrice.Location = New System.Drawing.Point(499, 316)
+        Me.lblPPrice.Name = "lblPPrice"
+        Me.lblPPrice.Size = New System.Drawing.Size(48, 13)
+        Me.lblPPrice.TabIndex = 12
+        Me.lblPPrice.Text = "lblPPrice"
+        '
+        'lblPurchaser
+        '
+        Me.lblPurchaser.AutoSize = True
+        Me.lblPurchaser.Location = New System.Drawing.Point(497, 366)
+        Me.lblPurchaser.Name = "lblPurchaser"
+        Me.lblPurchaser.Size = New System.Drawing.Size(65, 13)
+        Me.lblPurchaser.TabIndex = 13
+        Me.lblPurchaser.Text = "lblPurchaser"
+        '
+        'lblSalePrice
+        '
+        Me.lblSalePrice.AutoSize = True
+        Me.lblSalePrice.Location = New System.Drawing.Point(497, 408)
+        Me.lblSalePrice.Name = "lblSalePrice"
+        Me.lblSalePrice.Size = New System.Drawing.Size(62, 13)
+        Me.lblSalePrice.TabIndex = 14
+        Me.lblSalePrice.Text = "lblSalePrice"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label15.Location = New System.Drawing.Point(91, 461)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(99, 31)
+        Me.Label15.TabIndex = 15
+        Me.Label15.Text = "Rating"
+        '
+        'lblRating
+        '
+        Me.lblRating.AutoSize = True
+        Me.lblRating.Location = New System.Drawing.Point(501, 476)
+        Me.lblRating.Name = "lblRating"
+        Me.lblRating.Size = New System.Drawing.Size(48, 13)
+        Me.lblRating.TabIndex = 16
+        Me.lblRating.Text = "lblRating"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton1.Location = New System.Drawing.Point(815, 195)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(72, 35)
+        Me.RadioButton1.TabIndex = 17
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "NA"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton2.Location = New System.Drawing.Point(815, 240)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(106, 35)
+        Me.RadioButton2.TabIndex = 18
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "1 star"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton3.Location = New System.Drawing.Point(815, 281)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(106, 35)
+        Me.RadioButton3.TabIndex = 19
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "2 star"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton4.Location = New System.Drawing.Point(815, 316)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(106, 35)
+        Me.RadioButton4.TabIndex = 20
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "3 star"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'RadioButton5
+        '
+        Me.RadioButton5.AutoSize = True
+        Me.RadioButton5.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton5.Location = New System.Drawing.Point(815, 352)
+        Me.RadioButton5.Name = "RadioButton5"
+        Me.RadioButton5.Size = New System.Drawing.Size(106, 35)
+        Me.RadioButton5.TabIndex = 21
+        Me.RadioButton5.TabStop = True
+        Me.RadioButton5.Text = "4 star"
+        Me.RadioButton5.UseVisualStyleBackColor = True
+        '
+        'RadioButton6
+        '
+        Me.RadioButton6.AutoSize = True
+        Me.RadioButton6.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton6.Location = New System.Drawing.Point(815, 393)
+        Me.RadioButton6.Name = "RadioButton6"
+        Me.RadioButton6.Size = New System.Drawing.Size(106, 35)
+        Me.RadioButton6.TabIndex = 22
+        Me.RadioButton6.TabStop = True
+        Me.RadioButton6.Text = "5 star"
+        Me.RadioButton6.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(1034, 449)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(180, 127)
+        Me.Button2.TabIndex = 23
+        Me.Button2.Text = "Write new rating into external file"
+        Me.Button2.UseVisualStyleBackColor = True
         '
         'Form3
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1237, 602)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.RadioButton6)
+        Me.Controls.Add(Me.RadioButton5)
+        Me.Controls.Add(Me.RadioButton4)
+        Me.Controls.Add(Me.RadioButton3)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.lblRating)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.lblSalePrice)
+        Me.Controls.Add(Me.lblPurchaser)
+        Me.Controls.Add(Me.lblPPrice)
+        Me.Controls.Add(Me.lblSeller)
+        Me.Controls.Add(Me.lblSubject)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
@@ -98,4 +334,23 @@ Partial Class Form3
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents lblSubject As Label
+    Friend WithEvents lblSeller As Label
+    Friend WithEvents lblPPrice As Label
+    Friend WithEvents lblPurchaser As Label
+    Friend WithEvents lblSalePrice As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents lblRating As Label
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents RadioButton5 As RadioButton
+    Friend WithEvents RadioButton6 As RadioButton
+    Friend WithEvents Button2 As Button
 End Class
--- Form3.vb
+++ Form3.vb
@@ -1,59 +1,138 @@
 ﻿Public Class Form3
+    Dim bigarray(100, 200) As String 'declare array large enough for dataset
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
         'Prompt for search entries
-
-
         MsgBox("Loading file")
 
         'Open the database
         Dim sr As New System.IO.StreamReader("C:\presac1\shopdata.csv")
-        Dim linesInFile As Integer = 1
-        Dim x As Integer = 0
-        Dim textbook, subject, seller, pprice, sprice, purchaser, rating As String
-        'Label10.Text = x
 
+        '(columns, rows)
+        Dim linesInFile As Integer = 0
+        Dim columns As Integer
+
+        'Load the whole database into a 2 dimension array - bigarray
         Do Until sr.EndOfStream = True
-            linesInFile = linesInFile + 1
-            'label2 is the entire line
+
+            linesInFile = linesInFile + 1 ' indicates the number of rows, j
+
             Label2.Text = sr.ReadLine & vbNewLine
             'the test array splits the label2 into array items seperated by a comma
             Dim testArray() As String = Split(Label2.Text, Delimiter:=",")
-            For number As Integer = 0 To testArray.Length Step 1
-                textbook = testArray(0) ' textbook name
-                subject = testArray(1) ' subject name
-                seller = testArray(2)
-                pprice = testArray(3)
-                purchaser = testArray(4)
-                sprice = testArray(5)
-                rating = testArray(6)
+            'MsgBox(testArray.Length) ' 7 columns in 1 row. testArray.length=7
+
+            For columns = 0 To (testArray.Length - 1)
+                bigarray(columns, linesInFile) = testArray(columns)
             Next
-
-            MsgBox("Let's find our entry")
-
-            Dim textbooktofind, purchasertofind As String
-            textbooktofind = InputBox("What is the Name of the book?")
-            purchasertofind = InputBox("What is the first name Of the purchaser?")
-
-            'Search for stuff
-            Dim result1 As String() = Array.FindAll(testArray, Function(s) s.Contains(textbooktofind))
-            Dim result2 As String() = Array.FindAll(testArray, Function(s) s.Contains(purchasertofind))
-
-            'MsgBox(result1)
-            'MsgBox(result2)
-
-            'If result1.
-            'MsgBox(result1) 'Then
-            'MsgBox(result2)
-
-
-            'Label2.Text = testArray(0)
-            'Label7.Text = testArray(3)
-            'Label8.Text = testArray(5)
-            'Label4.Text = Val(testArray(5)) - Val(testArray(3))
-            'Label10.Text = Val(Label10.Text) + Val(Label4.Text)
-            MsgBox("Next?", vbOK)
-
         Loop
+        'Test if data sucessfully saved
+        MsgBox(bigarray(0, 2)) ' understanding silicon life
+        MsgBox(bigarray(0, 1)) ' This is the title row (not data)
+        MsgBox("Let's find our entry")
+
+        Dim textbooktofind, purchasertofind As String
+        textbooktofind = InputBox("What is the Name of the book?")
+        purchasertofind = InputBox("What is the first name Of the purchaser?")
+
+        Dim icount As Integer
+        Dim bfound As Boolean
+
+        MsgBox(linesInFile)
+        For icount = 2 To linesInFile
+            If bigarray(0, icount) = textbooktofind And bigarray(4, icount) = purchasertofind Then
+                bfound = True
+                MsgBox("Book found!")
+                lblTitle.Text = bigarray(0, icount)
+                lblSubject.Text = bigarray(1, icount)
+                lblSeller.Text = bigarray(2, icount)
+                lblPPrice.Text = bigarray(3, icount)
+                lblPurchaser.Text = bigarray(4, icount)
+                lblSalePrice.Text = bigarray(5, icount)
+                lblRating.Text = bigarray(6, icount)
+
+                MsgBox("Enter a new rating for the book on the right!")
+                RadioButton1.Enabled = True
+                RadioButton2.Enabled = True
+                RadioButton3.Enabled = True
+                RadioButton4.Enabled = True
+                RadioButton5.Enabled = True
+                RadioButton6.Enabled = True
+
+                If RadioButton1.Checked = True Then ' NA Rating
+                    bigarray(6, icount) = "NA"
+                ElseIf RadioButton2.Checked = True Then
+                    bigarray(6, icount) = "1"
+                ElseIf RadioButton3.Checked = True Then
+                    bigarray(6, icount) = "2"
+                ElseIf RadioButton4.Checked = True Then
+                    bigarray(6, icount) = "3"
+                ElseIf RadioButton5.Checked = True Then
+                    bigarray(6, icount) = "4"
+                ElseIf RadioButton6.Checked = True Then
+                    bigarray(6, icount) = "5"
+                Else
+                    MsgBox("Enter rating selection!")
+                End If
+                lblRating.Text = bigarray(6, icount)
+                'write this into new CSV file
+
+
+
+
+            Else
+                MsgBox("Book not found!")
+                Exit For
+            End If
+
+        Next
+
+        End
+
+
+    End Sub
+
+    Sub TwoDArrayToCSV(ByVal DataArray(,) As String)
+        Dim str As String = ""
+        Dim ofile As String = ""
+
+        svDialog("csv|*.csv", "save as...", ofile)
+        Dim sw As System.IO.StreamWriter = New System.IO.StreamWriter(ofile)
+
+        For i As Int32 = DataArray.GetLowerBound(0) To DataArray.GetUpperBound(0)
+            For j As Int32 = DataArray.GetLowerBound(1) To DataArray.GetUpperBound(1)
+                str += DataArray(i, j) + ","
+            Next
+            sw.WriteLine(str)
+            str = ""
+        Next
+        sw.Flush()
+        sw.Close()
+    End Sub
+
+    Sub svDialog(ByVal infilter As String, ByVal dtitle As String, ByRef outfile As String)
+        Dim openFileDialog1 As New SaveFileDialog()
+        With openFileDialog1
+            .Filter = infilter
+            .FilterIndex = 1
+            .Title = dtitle
+            .DefaultExt = Strings.Right(infilter, 3)
+            .ShowDialog()
+            outfile = openFileDialog1.FileName
+            .RestoreDirectory = True
+        End With
+    End Sub
+    Private Sub Form3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        RadioButton1.Enabled = False
+        RadioButton2.Enabled = False
+        RadioButton3.Enabled = False
+        RadioButton4.Enabled = False
+        RadioButton5.Enabled = False
+        RadioButton6.Enabled = False
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TwoDArrayToCSV(bigarray)
     End Sub
 End Class
